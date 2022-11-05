31c481df3c80abd109420b107367d83231705f99
Emerson66 <emersonreisdacosta@gmail.com>
2022-11-05 15:19:40 -0400

    banco de dados configurado

---
+++ cineminha/src/main/java/br/com/buritiscript/cineminha/CineminhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.cineminha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineminhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineminhaApplication.class, args);
+	}
+
+}
